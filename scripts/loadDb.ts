ce630f53dc87e7f02a3a45c3e1fa74331e93ef07
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2025-01-17 20:19:53 +0530

    update loadDb

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,7 +1,80 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
-import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer"
-import OpenAi from "openai"
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
-import "dotenv/config"
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import OpenAi from "openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+import "dotenv/config";
 
+type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
+
+const {
+  ASTRA_DB_NAMESPACE,
+  ASTRA_DB_COLLECTION,
+  ASTRA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN,
+  OPENAI_API_KEY,
+} = process.env;
+
+const openai = new OpenAi({ apiKey: OPENAI_API_KEY });
+
+const f1Data = [
+  "https://en.wikipedia.org/wiki/Formula_One",
+  "https://www.skysports.com/f1/opinion",
+  "https://www.skysports.com/f1/gossip",
+  "https://www.skysports.com/f1/news",
+  "https://www.forbes.com/sites/brettknight/2024/12/10/formula-1s-highest-paid-drivers-2024/",
+  "https://www.formula1.com/en/latest/all",
+  "https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship",
+  "https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship",
+  "https://en.wikipedia.org/wiki/2022_Formula_One_World_Championship",
+  "https://en.wikipedia.org/wiki/2021_Formula_One_World_Championship",
+  "https://www.formula1.com/en/results/2024/races",
+  "https://www.formula1.com/en/results/2023/races",
+  "https://www.formula1.com/en/results/2022/races",
+  "https://www.formula1.com/en/results/2021/races",
+  "https://www.formula1.com/en/results/2020/races",
+  "https://en.wikipedia.org/wiki/List_of_Formula_One_World_Drivers%27_Champions",
+];
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE });
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 100,
+});
+
+const createCollection = async (
+  similarityMetric: SimilarityMetric = "dot_product"
+) => {
+  const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+    vector: {
+      dimension: 1536,
+      metric: similarityMetric,
+    },
+  });
+  console.log(res);
+};
+
+const loadSampleData = async () => {
+  const collection = await db.collection(ASTRA_DB_COLLECTION);
+  for await (const url of f1Data) {
+    const content = await scrapePage(url);
+    const chunks = await splitter.splitText(content);
+    for await (const chunk of chunks) {
+      const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: "chunk",
+        encoding_format: "float",
+      });
+
+      const vector = embedding.data[0].embedding;
+
+      const res = await collection.insertOne({
+        $vector: vector,
+        text: chunk,
+      });
+      console.log(res);
+    }
+  }
+};
